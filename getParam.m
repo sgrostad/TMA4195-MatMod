85633275b182a6d65b96c127731f1e1f5389e00a
Esten Nicolai Wøien <esten.woien@gmail.com>
2018-11-13 14:04:13 +0100

    Oppdaterte konstanter.

--- getParam.m
+++ getParam.m
@@ -4,26 +4,25 @@ function [x0, xS, q0, a, m, J0, rho, kappa] = getParam(type)
             m  = 3.0;
             g  = 9.81;
             rho = 900;
-            
             L = 3400;
-            H = 7;
-            U = 50/(3600*24*365);
-            Q = U*H/L;
-            T = L/U;
-            mu = 2.4e-24;
+            H = 10;
+            mu = 1e-20;
             alpha = 15/180*pi;
             Theta = rho*g*H*sin(alpha);
             epsilon = H/L;
-            kappa = 2*epsilon*mu*H*Theta^m/Q;
             x0 = 0;
             xS = 0.0001;
             xF = 1;
-            q0 =   1 / (Q*3600*24*365*rho/1000);
-            q1 = -11 / (Q*3600*24*365*rho/1000);
+            q0 =  0.1;
+            q1 = -1.1;
             a  = (q0-q1)/(xF-xS);
-            J0 = rho;
-            Q*3600*24*365;
-            kappa/(m+2)*H;
+            kappa = (m+2)*(a/2-q0);
+            J0 = rho*kappa/(m+2);
+            
+            year = 3600*24*365;
+            Q = 2*epsilon*mu*H*Theta^m/kappa;
+            T = H/Q/year
+            U = L/T
            
         case 'Engabreen winter'
             m  = 3.0;
@@ -31,24 +30,21 @@ function [x0, xS, q0, a, m, J0, rho, kappa] = getParam(type)
             rho = 900;
             
             L = 3400;
-            H = 7;
-            U = 50/(3600*24*365);
-            Q = U*H/L;
-            T = L/U;
-            mu = 2.4e-24;
+            H = 15;
+            mu = 1e-20;
             alpha = 15/180*pi;
             Theta = rho*g*H*sin(alpha);
             epsilon = H/L;
-            kappa = 2*epsilon*mu*H*Theta^m/Q;
             x0 = 0;
             xS = 0.0001;
             xF = 1;
-            q0 =  5 / (Q*3600*24*365*rho/1000);
-            q1 = -7 / (Q*3600*24*365*rho/1000);
+            q0 =  0.1;
+            q1 = -1.1;
             a  = (q0-q1)/(xF-xS);
-            J0 = rho;
-            Q*3600*24*365;
-            kappa/(m+2)*H;
+            kappa = (m+2)*(a/2-q0);
+            J0 = rho*kappa/(m+2);
+            q0 =  0.5;
+            q1 = -0.7;
            
         case 'Engabreen summer'
             m  = 3.0;
@@ -56,24 +52,21 @@ function [x0, xS, q0, a, m, J0, rho, kappa] = getParam(type)
             rho = 900;
             
             L = 3400;
-            H = 7;
-            U = 50/(3600*24*365);
-            Q = U*H/L;
-            T = L/U;
+            H = 15;
             mu = 2.4e-24;
             alpha = 15/180*pi;
             Theta = rho*g*H*sin(alpha);
             epsilon = H/L;
-            kappa = 2*epsilon*mu*H*Theta^m/Q;
             x0 = 0;
             xS = 0.0001;
             xF = 1;
-            q0 = -3 / (Q*3600*24*365*rho/1000);
-            q1 = -15 / (Q*3600*24*365*rho/1000);
+            q0 =  0.1;
+            q1 = -1.1;
             a  = (q0-q1)/(xF-xS);
-            J0 = rho;
-            Q*3600*24*365;
-            kappa/(m+2)*H;
+            kappa = (m+2)*(a/2-q0);
+            J0 = rho*kappa/(m+2);
+            q0 = -0.3;
+            q1 = -1.5;
             
         case 'melting'
             x0 = 0;
--- testScriptTask14.m
+++ testScriptTask14.m
@@ -19,8 +19,8 @@ intq = @(x) getCumulativeAccumulationRate(x, x0, xS, xF, q0, a, J0, rho);
 
 
 % Bedrock profile
-d    = @(x) 0.0*(1+sin(pi*x/xS + 0));
-dddx = @(x) 0.0*pi/xS * cos(pi*x/xS + 0);
+d    = @(x) 0.02*(1-sin(3*pi*x - 0));
+dddx = @(x) -0.02*3*pi * cos(3*pi*x - 0);
 
 % Glacier height profile
 h    = @(x) getStationaryHeightProfile(x, intq, d, m, kappa, J0, rho);
@@ -31,7 +31,7 @@ dhdx = @(x) getStationaryHeightProfileDerivative(x, q, intq, d, dddx, m, kappa, 
 Nx = 1001;
 x = linspace(x0,xF+0.1*(xF-x0), Nx);
 
-figure
+
 plot(x, h(x), 'k', 'linewidth', 1)
 hold on
 plot(x, d(x), 'k', 'linewidth', 1)
@@ -39,21 +39,52 @@ axis equal
 
 
 
-dt = 0.1;
-dj = J0/20;
+dt = 0.01;
+dj = J0/10;
 u = @(x,z) getXVelocity(x, z, kappa, m, h, d);
 v = @(x,z) getZVelocity(x, z, kappa, m, h, d, dhdx, dddx);
 
 
 
 maxxqpos = getMaxXWithPositiveAccumulation(xS, q0, a);
-[c, t] = getStationaryTrajectories(u, v, h, d, dddx, intq, x0, m, kappa, rho, dt, dj, J0, maxxqpos);
+[c, tvec] = getStationaryTrajectories(u, v, h, d, dddx, intq, x0, m, kappa, rho, dt, dj, J0, maxxqpos);
+tvec
 
 
+% 
+% di = 50;
+% i = 1;
+% j = 50;
+% dlmwrite('quiver.txt', 'xyuv')
+% while true
+%     x = 
+%     dlmwrite('quiver.txt', [x,z,u(x,z),v(x,z)], '-append')
+% end
+
+
+xh = [x.', h(x).'];
+xd = [x.', d(x).'];
+dlmwrite('stationary.txt', 'xy')
+dlmwrite('stationary.txt', xh, '-append')
+dlmwrite('bedrock.txt', 'xy')
+dlmwrite('bedrock.txt', xd, '-append')
 for i = 1:length(c)
     xz = c{i}(:,1:5:end);
+    dlmwrite(sprintf('traj%i.txt', i), 'xy');
+    dlmwrite(sprintf('traj%i.txt', i), xz.', '-append');
     %plot(xz(1,:), xz(2,:), 'k');
     %quiver(xz(1,:), xz(2,:), u(xz(1,:), xz(2,:)), v(xz(1,:), xz(2,:)), 0.2, 'k', 'markersize', 200);
 end
+
+
+
+dx = 0.1;
+dlmwrite('vectorfield.txt', 'xyuv')
+for x = dx/2:dx:1-dx/2
+    for z = dx/2:dx:h(x)-dx/2
+        dlmwrite('vectorfield.txt', [x,z,u(x,z),v(x,z)], '-append')
+    end
+end
+
 
 
